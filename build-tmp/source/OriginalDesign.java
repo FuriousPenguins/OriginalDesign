6f559ef00225f9e07f8b9f54f9af244a014b9e10
FuriousPenguins <jiamingqiu951@gmail.com>
2015-08-26 10:46:42 -0700

    Removed 3D and added a new project because 3D doesn't work

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,66 +14,21 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-int red = 127;
-int blue = 127;
-int green = 127;
+int a=100;
+int b;
+int x = 100;
+int y = 100;
 
 public void setup() {
-  size(800, 400, P3D);
-  background(0);
-  // camera(0,0,200,0,0,0, 0.0, 1.0, 0.0);
-  noStroke();
+	size(200,200);
+	background(255);
 }
 
 public void draw() {
-  lights();
-  red += random(-5,6);
-  blue += random(-5,6);
-  green += random(-5,6);
-  pointLight(red,green,blue,400,200,150);
-  translate(400,200,150);
-  pushMatrix();
-  translate(-60, 0, 0);
-  sphere(30);
-  popMatrix();
-
-  pushMatrix();
-  translate(60, 0, 0);
-  sphere(30);
-  popMatrix();
-
-  pushMatrix();
-  translate(60,-60,-60);
-  sphere(30);
-  popMatrix();
-
-  pushMatrix();
-  translate(-60,-60,-60);
-  sphere(30);
-  popMatrix();
-
-  pushMatrix();
-  translate(60,60,-60);
-  sphere(30);
-  popMatrix();
-
-  pushMatrix();
-  translate(-60,60,-60);
-  sphere(30);
-  popMatrix();
-
-  pushMatrix();
-  translate(0,-60,0);
-  sphere(30);
-  popMatrix();
-
-  pushMatrix();
-  translate(0,60,0);
-  sphere(30);
-  popMatrix();
-
-  textSize(16);
-  text("The balls will slowly change color", -130,-100,0);
+	x -= 1;
+	b += 1;
+	ellipse(100,100,x,y);
+	ellipse(100,100,a,b);
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
