e7c65d3d4e7b27367beadbd7d55a269db131edef
FuriousPenguins <jiamingqiu951@gmail.com>
2015-08-26 10:20:23 -0700

    Completed Project

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,15 +1,31 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
 int red = 127;
 int blue = 127;
 int green = 127;
 
-void setup() {
+public void setup() {
   size(800, 400, P3D);
   background(0);
-  camera(0,0,200,0,0,0, 0.0, 1.0, 0.0);
+  camera(0,0,200,0,0,0, 0.0f, 1.0f, 0.0f);
   noStroke();
 }
 
-void draw() {
+public void draw() {
   lights();
   red += random(-5,6);
   blue += random(-5,6);
@@ -57,4 +73,13 @@ void draw() {
 
   textSize(16);
   text("The balls will slowly change color", -130,-100,0);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
