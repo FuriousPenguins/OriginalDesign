c8b1d34585310ce0ba36d02ca2abd02853b6e3b4
unknown <jiqiu@697-334-Bard.sf.k12.internal>
2015-08-27 10:19:36 -0700

    Added a few more ellipses and changed stroke color

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,16 +1,51 @@
-int a=100;
-int b;
-int x = 100;
-int y = 100;
+// int a=100;
+int e1x;
+int e1y;
+
+int ex1;
+int ey1;
+
+int red;
+int green;
+int blue;
 
 void setup() {
 	size(200,200);
 	background(255);
+	frameRate(30);
 }
 
 void draw() {
-	x -= 1;
-	b += 1;
-	ellipse(100,100,x,y);
-	ellipse(100,100,a,b);
+	noFill();
+	randomStroke();
+	stroke(red,green,blue);
+	drawEllipse1();
+	if (e1x == 200) {
+		draw4Ellipse();
+	}
+}
+
+void drawEllipse1() {
+	ellipse(100,100,e1x,e1y);
+	e1y += 1;
+	if(e1x < 200) {
+		e1x += 2;
+		System.out.println(e1x);
+	}
+}
+
+void draw4Ellipse() {
+	ellipse(0,0,ex1,ey1);
+	ellipse(200,200,ex1,ey1);
+	ellipse(0,200,ex1,ey1);
+	ellipse(200,0,ex1,ey1);
+	
+	ex1 += 1;
+	ey1 += 1;
+}
+
+void randomStroke() {
+	red = int(random(0,256));
+	green = int(random(0,256));
+	blue = int(random(0,256));
 }
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,21 +14,56 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-int a=100;
-int b;
-int x = 100;
-int y = 100;
+// int a=100;
+int e1x;
+int e1y;
+
+int ex1;
+int ey1;
+
+int red;
+int green;
+int blue;
 
 public void setup() {
 	size(200,200);
 	background(255);
+	frameRate(30);
 }
 
 public void draw() {
-	x -= 1;
-	b += 1;
-	ellipse(100,100,x,y);
-	ellipse(100,100,a,b);
+	noFill();
+	randomStroke();
+	stroke(red,green,blue);
+	drawEllipse1();
+	if (e1x == 200) {
+		draw4Ellipse();
+	}
+}
+
+public void drawEllipse1() {
+	ellipse(100,100,e1x,e1y);
+	e1y += 1;
+	if(e1x < 200) {
+		e1x += 2;
+		System.out.println(e1x);
+	}
+}
+
+public void draw4Ellipse() {
+	ellipse(0,0,ex1,ey1);
+	ellipse(200,200,ex1,ey1);
+	ellipse(0,200,ex1,ey1);
+	ellipse(200,0,ex1,ey1);
+	
+	ex1 += 1;
+	ey1 += 1;
+}
+
+public void randomStroke() {
+	red = PApplet.parseInt(random(0,256));
+	green = PApplet.parseInt(random(0,256));
+	blue = PApplet.parseInt(random(0,256));
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
